17265687b5a56023759a374c75bc4957e38f7606
cesheard <cesheard@hotmail.com>
2022-08-17 02:21:57 -0400

    Started Setting Base Mechanics
    I have a 3D background working and made 2D spite assets for the plant placeholders, which contain a pot and a plant.
    
    Added a GameManager Script. I'm currently thinking of having the GameManager randomly assign the pot and plant from a list/array. The GameManager would then also keep track of growth and when the plant can be used, as well as the customer's requests.
    
    Tried making a Plant ScriptableObject, but it's not working; still trying to figure out why.

---
+++ Gias Greenhouse/Assets/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] public GameObject[] plantPlaceholders;
+    [SerializeField] public GameObject[] plantTypes;
+    [SerializeField] public GameObject[] potTypes;
+
+    private void Awake()
+    {
+        
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
