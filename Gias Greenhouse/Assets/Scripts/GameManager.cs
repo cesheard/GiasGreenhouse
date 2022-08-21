f158ddf6dd321ad11dc983e881dca9a50ab1eaa3
cesheard <cesheard@hotmail.com>
2022-08-21 18:37:51 -0400

    Plants Grow with time
    Needs some tweeking and revisions, but it works, kind of

--- Gias Greenhouse/Assets/Scripts/GameManager.cs
+++ Gias Greenhouse/Assets/Scripts/GameManager.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using TMPro;
@@ -17,8 +18,12 @@ public class GameManager : MonoBehaviour
         foreach (PlantPlaceholderScript plantPlaceholder in plantPlaceholders)
         {
             plantPlaceholder.assignedPlant = plantTypes[Random.Range(0, plantTypes.Length)];
-            plantPlaceholder.assignedPlantSprite = plantPlaceholder.assignedPlant.stages[Random.Range(0, 4)];
+            plantPlaceholder.assignedPlantCurrentStage = Random.Range(0, 4);
+            plantPlaceholder.assignedPlantSprite = plantPlaceholder.assignedPlant.stages[plantPlaceholder.assignedPlantCurrentStage];
             plantPlaceholder.GetComponentInChildren<SpriteRenderer>().sprite = plantPlaceholder.assignedPlantSprite;
+
+            Debug.Log(plantPlaceholders[0].assignedPlantCurrentStage + " " + plantPlaceholders[0].assignedPlantSprite.name);
+            StartCoroutine(plantPlaceholder.GrowTime(plantPlaceholder.assignedPlant.growTime));
         }
 
     } // End of Awake()
@@ -28,6 +33,12 @@ public class GameManager : MonoBehaviour
         pauseMenu = FindObjectOfType<PauseMenu>();
 
     } // End of Start()
+
+    void Update()
+    {
+        StartCoroutine(DoCheck());
+
+    } // End of Update()
 
     public void OnPauseMenu(InputValue value)
     {
@@ -49,4 +60,61 @@ public class GameManager : MonoBehaviour
         scoreText.text = "You correctly labeled " + correctLabelCount + " out of " + plantPlaceholders.Length;
 
     } // End of CheckLabels()
+
+    // Do a check every tenth of a second
+    IEnumerator DoCheck()
+    {
+        for (; ; )
+        {
+            // Check growth of each plant
+            foreach (PlantPlaceholderScript plantPlaceholder in plantPlaceholders)
+            {
+                if (plantPlaceholder.stageDone && plantPlaceholder.assignedPlantCurrentStage < 3)
+                {
+                    // Add needs water indication
+
+                    // When watered, start next grow stage
+                    plantPlaceholder.stageDone = false;
+                    plantPlaceholder.assignedPlantCurrentStage++;
+                    plantPlaceholder.assignedPlantSprite = plantPlaceholder.assignedPlant.stages[plantPlaceholder.assignedPlantCurrentStage];
+                    plantPlaceholder.GetComponentInChildren<SpriteRenderer>().sprite = plantPlaceholder.assignedPlantSprite;
+
+                    StartCoroutine(plantPlaceholder.GrowTime(plantPlaceholder.assignedPlant.growTime));
+                }
+                if (plantPlaceholder.stageDone && plantPlaceholder.assignedPlantCurrentStage == 3)
+                {
+                    // Add ready to pick indication
+
+                    // When re-planted, start first grow stage
+                    //plantPlaceholder.assignedPlantCurrentStage = 0;
+                }
+            }
+
+
+
+            /*if (plantPlaceholders[0].stageDone && plantPlaceholders[0].assignedPlantCurrentStage < 3)
+            {
+                // Add needs water indication
+
+                // When watered, start next grow stage
+                plantPlaceholders[0].stageDone = false;
+                plantPlaceholders[0].assignedPlantCurrentStage++;
+                plantPlaceholders[0].assignedPlantSprite = plantPlaceholders[0].assignedPlant.stages[plantPlaceholders[0].assignedPlantCurrentStage];
+                plantPlaceholders[0].GetComponentInChildren<SpriteRenderer>().sprite = plantPlaceholders[0].assignedPlantSprite;
+
+                Debug.Log(plantPlaceholders[0].assignedPlantCurrentStage + " " + plantPlaceholders[0].assignedPlantSprite.name);
+                StartCoroutine(plantPlaceholders[0].GrowTime(plantPlaceholders[0].assignedPlant.growTime));
+            }
+            if (plantPlaceholders[0].stageDone && plantPlaceholders[0].assignedPlantCurrentStage == 3)
+            {
+                // Add ready to pick indication
+
+                // When re-planted, start first grow stage
+                //plantPlaceholder.assignedPlantCurrentStage = 0;
+                Debug.Log(plantPlaceholders[0].assignedPlantCurrentStage + " " + plantPlaceholders[0].assignedPlantSprite.name);
+            }*/
+
+            yield return new WaitForSeconds(.1f);
+        }
+    }
 }
--- Gias Greenhouse/Assets/Scripts/PlantsAndPlanters/PlantPlaceholderScript.cs
+++ Gias Greenhouse/Assets/Scripts/PlantsAndPlanters/PlantPlaceholderScript.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PlantPlaceholderScript : MonoBehaviour
@@ -6,10 +8,20 @@ public class PlantPlaceholderScript : MonoBehaviour
     public Plant assignedPlant;
     public Sprite assignedPlantSprite;
     public DropSlot labelSlot;
+    public int assignedPlantCurrentStage = -1;
+    public bool stageDone = false;
 
     // Checks to see if the assigned label is correct, returns bool; GameManager references the function
     public bool CheckLabels()
     {
         return (labelSlot.labelName.Equals(assignedPlant.name));
     } // End of CheckLabels()
+
+    public IEnumerator GrowTime(float growTime)
+    {
+        stageDone = false;
+        yield return new WaitForSeconds(growTime);
+
+        stageDone = true;
+    } // End of GrowTime(float growTime)
 }
